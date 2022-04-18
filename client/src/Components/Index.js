b54604458ec78b3ee4cd91dc2c7ca8498189d2ca
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-18 13:56:58 +0430

    add dummy data file

--- client/src/Components/Index.js
+++ client/src/Components/Index.js
@@ -5,3 +5,4 @@ export { default as Services } from "./Services";
 export { default as Transactions } from "./Transactions";
 export { default as Welcome } from "./Welcome";
 export { default as Input } from "./Input";
+export { default as ServiceCard } from "./ServiceCard";
