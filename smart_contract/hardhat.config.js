53d41cca2d7eb432e2b9ed4ae6e0bae1b7565d62
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-18 11:45:53 +0430

    add smart contract files

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,16 +1,13 @@
-// https://eth-ropsten.alchemyapi.io/v2/8dTI7nNb1oSm4c_lr4G3Q1_T4I54pcBy
-
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/8dTI7nNb1oSm4c_lr4G3Q1_T4I54pcBy",
+      url: "https://eth-ropsten.alchemyapi.io/v2/3Nps1rAGoRlvOWpz_MVy6F8KwOypTUeT",
       accounts: [
         "4785cd0f34d37a2987cb97c5a1987dbdefec317e9b9bfb604897f8441d0bef54",
       ],
     },
   },
- 
 };
